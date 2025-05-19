66473b882f13bc203c2b517973aeb6b54f94abf5
Remco Tolsma <info@remcotolsma.nl>
2025-05-19 15:18:08 +0200

    Update test.php

--- tests/test.php
+++ tests/test.php
@@ -6,3 +6,7 @@ __( 'Test 1', 'old' );
 test__( 'Test 3', 'old' );
 
 __( 'Test 4', 'older' );
+
+load_plugin_textdomain( 'old', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+load_plugin_textdomain( 'old', false );
+load_plugin_textdomain( 'old' );
